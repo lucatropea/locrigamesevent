f1e9cbb6430c4b0caebbe78c835d5667278634f4
lucatropea <info@lucatropea.it>
2021-10-11 17:06:08 +0200

    Basic homepage with our logo

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home flex w-full h-screen">
+    <img class="m-auto w-full p-8 md:p-0 md:w-1/2" salt="Locri Games Event" src="@/assets/images/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
